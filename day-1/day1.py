0d5a364583ede93fc86709918cfb04e90be802c1
yunko006 <peyrachethomas@gmail.com>
2021-12-15 11:57:02 +0100

    Update day1.py

--- day-1/day1.py
+++ day-1/day1.py
@@ -12,11 +12,8 @@ def part1(data):
 
 
 def window(data):
-    l = list()
-    # si i = 1: window = 1, 2, 3 // si i = 2, window = 2,3,4 etc
-    for i in range(1, len(data) - 1):
-        l.append(data[i - 1] + data[i] + data[i + 1])
-    return l
+
+    return [data[i - 1] + data[i] + data[i + 1] for i in range(1, len(data) - 1)]
 
 
 if __name__ == "__main__":
@@ -24,7 +21,8 @@ if __name__ == "__main__":
     lines = pathlib.Path('day1_data.txt').read_text().rstrip().split('\n')
     puzzle_input = [int(line) for line in lines]
 
-    print(p1 = part1(puzzle_input))
+    p1 = part1(puzzle_input)
+    print(p1)
 
     w = window(puzzle_input)
     p2 = part1(w)
